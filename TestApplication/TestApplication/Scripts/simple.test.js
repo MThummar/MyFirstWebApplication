617e286ff147f1f25175b83434f8e33496d001f4
MThummar <ing.thummar@yahoo.com>
2017-01-30 13:00:24 +0100

    Added file simple.test.js Chnage Background Color using external file

---
+++ TestApplication/TestApplication/Scripts/simple.test.js
@@ -0,0 +1,11 @@
+﻿function setColor() {
+    var colors = ["red", "pink", "yellow", "green", "black", "blue"], color;
+
+    var randColor;
+    do {
+        randColor = colors[Math.floor(Math.random() * colors.length)];
+    } while (color == randColor);
+    document.body.style.backgroundColor = randColor;
+    this.style.backgroundColor = randColor;
+
+}
